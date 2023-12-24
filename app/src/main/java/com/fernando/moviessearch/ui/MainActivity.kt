e23b3ac6a5f0863d39e5a8f8bf4c0f4966302a83
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-12-23 22:23:41 -0300

    Create ui package and move MainActivity to ui package

--- app/src/main/java/com/fernando/moviessearch/ui/MainActivity.kt
+++ app/src/main/java/com/fernando/moviessearch/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.fernando.moviessearch
+package com.fernando.moviessearch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fernando.moviessearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
