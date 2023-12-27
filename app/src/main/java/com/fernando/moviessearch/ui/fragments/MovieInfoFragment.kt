ddcfb2fa56ad86bebd778e60fea4002a66adba1b
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-12-27 20:45:14 -0300

    Update MovieInfoFragment

--- app/src/main/java/com/fernando/moviessearch/ui/fragments/MovieInfoFragment.kt
+++ app/src/main/java/com/fernando/moviessearch/ui/fragments/MovieInfoFragment.kt
@@ -1,10 +1,14 @@
 package com.fernando.moviessearch.ui.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.transition.Visibility
+import com.bumptech.glide.Glide
+import com.fernando.moviessearch.R
 import com.fernando.moviessearch.databinding.FragmentMovieInfoBinding
 import com.fernando.moviessearch.ui.utils.FragmentViewBinding
 import com.fernando.moviessearch.ui.viewModels.MovieViewModel
@@ -19,6 +23,7 @@ class MovieInfoFragment : FragmentViewBinding<FragmentMovieInfoBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        showMovieInfoSkeleton()
         requestMovie(movieInfoArgs.movieID)
         initObservables()
     }
@@ -27,9 +32,40 @@ class MovieInfoFragment : FragmentViewBinding<FragmentMovieInfoBinding>() {
         movieViewModel.setMovie(movieID)
     }
 
+    private fun showMovieInfoSkeleton() {
+        binding.movieSkeletonLinearLayout.visibility = View.VISIBLE
+        binding.movieInfoLinearLayout.visibility = View.GONE
+    }
+
+    private fun showMovieInfo() {
+        binding.movieInfoLinearLayout.visibility = View.VISIBLE
+        binding.movieSkeletonLinearLayout.visibility = View.GONE
+    }
+
     private fun initObservables() {
         movieViewModel.movie.observe(requireActivity()) { movie ->
-            println(movie)
+            showMovieInfo()
+            binding.apply {
+                val movieImageURL = "https://image.tmdb.org/t/p/original/${movie.posterImageUrl}"
+                val movieReleaseYear = movie.releaseDate.year.toString()
+
+                movieTitleTextView.text = movie.title
+                movieReleaseYearTextView.text = movieReleaseYear
+                if (movie.sinopse.isNotEmpty()) {
+                    movieSinopseTextView.text = movie.sinopse
+                }
+                else {
+                    movieSinopseTextView.text = "Sinopse indisponivel"
+                }
+                setMovieImage(movieImageURL)
+            }
         }
+    }
+
+    private fun setMovieImage(imageURL: String) {
+        Glide.with(binding.root.context)
+            .load(imageURL)
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(binding.moviePosterImageView)
     }
 }
