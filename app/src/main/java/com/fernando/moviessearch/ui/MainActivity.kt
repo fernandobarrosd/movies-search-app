f317ea050c1378c8d6dd2451cb5b10ff0ab98507
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-12-27 20:48:43 -0300

    Update MainActivity file

--- app/src/main/java/com/fernando/moviessearch/ui/MainActivity.kt
+++ app/src/main/java/com/fernando/moviessearch/ui/MainActivity.kt
@@ -2,11 +2,29 @@ package com.fernando.moviessearch.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.fernando.moviessearch.R
+import com.fernando.moviessearch.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initToolbar()
+    }
+
+    private fun initToolbar() {
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id)
+        as NavHostFragment
+
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
