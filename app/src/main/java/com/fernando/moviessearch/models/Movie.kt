d5068eb8fe0623030d9d44fec26159fdeb38805e
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2023-12-27 20:44:22 -0300

    Update Movie model

--- app/src/main/java/com/fernando/moviessearch/models/Movie.kt
+++ app/src/main/java/com/fernando/moviessearch/models/Movie.kt
@@ -2,11 +2,23 @@ package com.fernando.moviessearch.models
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fernando.moviessearch.deserializers.LocalDateDeserializer
+import com.fernando.moviessearch.serializers.LocalDateSerializer
+import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Movie(
     @JsonProperty("id") val id: Long,
     @JsonProperty("title") val title: String,
-    @JsonProperty("release_date") val releaseDate: String,
-    @JsonProperty("poster_path") val posterImageUrl: String
+
+    @JsonProperty("release_date")
+    @JsonDeserialize(using = LocalDateDeserializer::class)
+    @JsonSerialize(using = LocalDateSerializer::class)
+    val releaseDate: LocalDate,
+    @JsonProperty("poster_path") val posterImageUrl: String,
+
+    @JsonProperty("overview")
+    val sinopse: String
 )
